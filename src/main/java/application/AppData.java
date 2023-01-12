4c085b4b741e7c6dfca1b1aa78c09bcef82cf329
Ghislain Demaël <58844111+LeCarnotzetVeveysan@users.noreply.github.com>
2023-01-12 15:56:13 +0100

    AppData addition

--- src/main/java/application/AppData.java
+++ src/main/java/application/AppData.java
@@ -4,10 +4,13 @@ public class AppData {
 
     private static int prefWidth;
     private static int prefHeight;
+    private static String language;
+
 
     public AppData() {
         prefWidth = 600;
         prefHeight = 400;
+        language = "english";
     }
 
     public static int getPrefWidth(){
