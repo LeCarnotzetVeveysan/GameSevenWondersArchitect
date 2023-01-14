8c28e58e32671a675e8ce68694199a7973784549
Ghislain Demaël <58844111+LeCarnotzetVeveysan@users.noreply.github.com>
2023-01-14 06:30:03 +0100

    GameSceneController - FXML variable cleanup

--- src/main/java/controllers/GameSceneController.java
+++ src/main/java/controllers/GameSceneController.java
@@ -11,124 +11,28 @@ import javafx.scene.input.MouseEvent;
 public class GameSceneController {
 
     @FXML
-    private Button ButtonScience1;
+    private Button ButtonScience1, ButtonScience2, ButtonScience3, ButtonScienceRand;
 
     @FXML
-    private Button ButtonScience2;
+    private ImageView ImageViewJetonsPaix1, ImageViewJetonsPaix2, ImageViewJetonsPaix3;
 
     @FXML
-    private Button ButtonScience3;
+    private ImageView ImageViewJetonsSciences1, ImageViewJetonsSciences2, ImageViewJetonsSciences3, ImageViewJetonsSciencesRand;
 
     @FXML
-    private Button ButtonScienceRand;
+    private Button NextTurnButton, PiocheCentraleButton, PiocheDroiteButton, PiocheGaucheButton;
 
     @FXML
-    private ImageView ImageViewJetonsPaix1;
+    private Label Player1Name, Player2Name, Player3Name, Player4Name, Player5Name, Player6Name, Player7Name;
 
     @FXML
-    private ImageView ImageViewJetonsPaix2;
+    private TitledPane HoverPane1, HoverPane2, HoverPane3, HoverPane4, HoverPane5, HoverPane6, HoverPane7;
 
     @FXML
-    private ImageView ImageViewJetonsPaix3;
+    private Label InfoPlayer1, InfoPlayer2, InfoPlayer3, InfoPlayer4, InfoPlayer5, InfoPlayer6, InfoPlayer7;
 
     @FXML
-    private ImageView ImageViewJetonsSciences1;
-
-    @FXML
-    private ImageView ImageViewJetonsSciences2;
-
-    @FXML
-    private ImageView ImageViewJetonsSciences3;
-
-    @FXML
-    private ImageView ImageViewJetonsSciencesRand;
-
-    @FXML
-    private Button NextTurnButton;
-
-    @FXML
-    private Button PiocheCentraleButton;
-
-    @FXML
-    private Button PiocheDroiteButton;
-
-    @FXML
-    private Button PiocheGaucheButton;
-
-    @FXML
-    private Label Player1Name;
-
-    @FXML
-    private Label Player2Name;
-
-    @FXML
-    private Label Player3Name;
-
-    @FXML
-    private Label Player4Name;
-
-    @FXML
-    private Label Player5Name;
-
-    @FXML
-    private Label Player6Name;
-
-    @FXML
-    private Label Player7Name;
-
-    @FXML
-    private TitledPane HoverPane1;
-
-    @FXML
-    private TitledPane HoverPane2;
-
-    @FXML
-    private TitledPane HoverPane3;
-
-    @FXML
-    private TitledPane HoverPane4;
-
-    @FXML
-    private TitledPane HoverPane5;
-
-    @FXML
-    private TitledPane HoverPane6;
-
-    @FXML
-    private TitledPane HoverPane7;
-
-    @FXML
-    private Label InfoPlayer1;
-
-    @FXML
-    private Label InfoPlayer2;
-
-    @FXML
-    private Label InfoPlayer3;
-
-    @FXML
-    private Label InfoPlayer4;
-
-    @FXML
-    private Label InfoPlayer5;
-
-    @FXML
-    private Label InfoPlayer6;
-
-    @FXML
-    private Label InfoPlayer7;
-
-    @FXML
-    private Label Materials;
-
-    @FXML
-    private Label WarPoints;
-
-    @FXML
-    private Label WinPoints;
-
-    @FXML
-    private Label JetonsSciencesJoueur;
+    private Label Materials, WarPoints, WinPoints, JetonsSciencesJoueur;
 
     @FXML
     void PiocheCentraleClicked(ActionEvent event) {
