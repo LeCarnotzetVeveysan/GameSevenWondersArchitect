de667c42945ba6d5dda2d119a7ce702ca0e35674
Redhoxx <64523199+Redhoxx@users.noreply.github.com>
2023-01-18 21:54:05 +0100

    Delete src/main/java/classes directory

--- src/main/java/classes/LaurelToken.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class LaurelToken {
-
-    int points;
-    int color;
-
-}
--- src/main/java/classes/MaterialToken.java
+++
@@ -1,8 +0,0 @@
-package classes;
-
-public class MaterialToken {
-
-    String type;
-    int color;
-
-}
